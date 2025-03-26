6b402a3504dfbed68dd844035bbfe93834ed1c16
Adrien Gentizon <adrien.gentizon@gmail.com>
2025-03-26 20:07:25 +0100

    Update RandomBackground.tsx

--- components/RandomBackground/RandomBackground.tsx
+++ components/RandomBackground/RandomBackground.tsx
@@ -59,7 +59,7 @@ export default function RandomBackground() {
           );
         }
       }}
-      className="hidden absolute inset-[-50px] -z-10 sm:grid"
+      className="absolute inset-[-50px] -z-10 grid"
       style={{
         gridRow: COLS,
       }}
