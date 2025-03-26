2662b3eda64f772727c5d3b86e2e5809777b990d
Adrien Gentizon <adrien.gentizon@gmail.com>
2025-03-26 17:46:45 +0100

    add custom link pngs

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
 import Logo from "../Logo/logo";
 import isDevPlatform from "@/lib/isDevPlatform";
+import AgendaPNG from "./agenda.png";
+import DrugstorePNG from "./drugstore.png";
+import ContactPNG from "./contact.png";
+import Image from "next/image";
 
 function Title() {
   return (
@@ -20,15 +24,39 @@ export default function Header() {
       </Link>
       {isDevPlatform() && (
         <nav>
-          <ul className="flex items-center justify-center gap-4 text-sm font-light uppercase">
+          <ul className="flex cursor-pointer items-center justify-center gap-4 pt-4 text-sm font-light uppercase">
             <li>
-              <Link href={`/`}>Agenda</Link>
+              <Link href={`/`}>
+                <Image
+                  src={AgendaPNG}
+                  alt="Agenda"
+                  className="w-24 sm:w-40"
+                  role="button"
+                />
+                <span className="sr-only">Agenda</span>
+              </Link>
             </li>
             <li>
-              <Link href={`/drugstore`}>Drugstore</Link>
+              <Link href={`/drugstore`}>
+                <Image
+                  src={DrugstorePNG}
+                  alt="Drugstore"
+                  className="w-24 sm:w-40"
+                  role="button"
+                />
+                <span className="sr-only">Drugstore</span>
+              </Link>
             </li>
             <li>
-              <Link href={`/contact`}>Contact</Link>
+              <Link href={`/contact`}>
+                <Image
+                  src={ContactPNG}
+                  alt="Contact"
+                  className="w-24 sm:w-40"
+                  role="button"
+                />
+                <span className="sr-only">Contact</span>
+              </Link>
             </li>
           </ul>
         </nav>
