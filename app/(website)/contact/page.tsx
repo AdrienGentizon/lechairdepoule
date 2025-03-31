91ea8d58c74b9709bed94e3874c2cfb67208effc
Adrien Gentizon <adrien.gentizon@gmail.com>
2025-03-31 16:51:15 +0200

    hack contact page layout

--- app/(website)/contact/page.tsx
+++ app/(website)/contact/page.tsx
@@ -9,36 +9,40 @@ export default async function ConctactPage() {
   if (!contactPage) return <></>;
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4 bg-black">
-      <div className="flex flex-col items-center justify-center pt-4 sm:pb-4 sm:pt-8">
-        <h2 className="sr-only">Adresse</h2>
-        <div className="font-mono">
-          <Markdown>{contactPage.contactNumbers}</Markdown>
-        </div>
-      </div>
+    <>
+      <div />
 
-      <div className="grid grid-cols-1 gap-0 sm:grid-cols-2 sm:gap-x-16">
-        <div className="flex flex-col items-center justify-center p-2">
-          <h2>
-            <span className="sr-only">Le Chair de Poule</span>
-            <ChairDePoule className="w-24" />
-          </h2>
+      <div className="flex flex-col items-center justify-center gap-4 bg-black">
+        <div className="flex flex-col items-center justify-center pt-4 sm:pb-4 sm:pt-8">
+          <h2 className="sr-only">Adresse</h2>
           <div className="font-mono">
-            <Markdown>{contactPage.chairdepouleOpeningHours}</Markdown>
+            <Markdown>{contactPage.contactNumbers}</Markdown>
           </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center p-2">
-          <h2>
-            <span className="sr-only">Le Peine Perdue</span>
-            <PeinePerdue className="w-24" />
-          </h2>
-          <div className="font-mono">
-            <Markdown>{contactPage.peineperdueOpeningHours}</Markdown>
+        <div className="grid grid-cols-1 gap-0 sm:grid-cols-2 sm:gap-x-16">
+          <div className="flex flex-col items-center justify-center p-2">
+            <h2>
+              <span className="sr-only">Le Chair de Poule</span>
+              <ChairDePoule className="w-24" />
+            </h2>
+            <div className="font-mono">
+              <Markdown>{contactPage.chairdepouleOpeningHours}</Markdown>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center p-2">
+            <h2>
+              <span className="sr-only">Le Peine Perdue</span>
+              <PeinePerdue className="w-24" />
+            </h2>
+            <div className="font-mono">
+              <Markdown>{contactPage.peineperdueOpeningHours}</Markdown>
+            </div>
           </div>
         </div>
+        <ContactForm />
       </div>
-      <ContactForm />
-    </div>
+    </>
   );
 }
