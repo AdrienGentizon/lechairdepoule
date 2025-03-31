077c7a8009801c1de9da0d5cb41dbf6aa956b4d2
Adrien Gentizon <adrien.gentizon@gmail.com>
2025-03-31 16:10:26 +0200

    fix footer position

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full bg-black py-1 text-center sm:max-w-2xl">
+    <footer className="mt-auto h-min w-full bg-black py-1 text-center sm:max-w-2xl">
       <p className="text-xs font-light">
         fabriqué par Brian
         <strong className="font-medium">&</strong>
