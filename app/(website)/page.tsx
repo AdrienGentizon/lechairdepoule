f1b7999fd4e915dc6a9459efc2c66930297e353c
Adrien Gentizon <adrien.gentizon@gmail.com>
2025-04-17 18:14:45 +0200

    revalidate every 6 hours

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -2,6 +2,8 @@ import EventsList from "@/components/EventsList/EventsList";
 import getEvents from "../../queries/getEvents";
 import { Suspense } from "react";
 
+export const revalidate = 60 * 60 * 6;
+
 export default async function Home() {
   const events = await getEvents();
   return (
