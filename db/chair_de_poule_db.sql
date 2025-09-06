d1c66a2eb0a59e8c56374b70af50c1653a2b4e1c
Adrien Gentizon <adrien.gentizon@gmail.com>
2025-09-06 16:32:53 +0200

    update db

--- db/chair_de_poule_db.sql
+++ db/chair_de_poule_db.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
+    pseudo TEXT NOT NULL,
     created TIMESTAMP,
     last_connection TIMESTAMP
 );
