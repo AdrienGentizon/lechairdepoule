9a02fd643f54cc8b7244475774ac037299cf3dd0
Adrien Gentizon <adrien.gentizon@gmail.com>
2025-10-05 13:00:53 +0200

    improve forum layout

--- app/(forum)/layout.tsx
+++ app/(forum)/layout.tsx
@@ -2,7 +2,6 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import RandomBackground from "@/components/RandomBackground/RandomBackground";
 import ReactQueryProvider from "@/contexts/ReactQueryProvider";
-import { cn } from "@/lib/utils";
 import getRandomBackground from "@/queries/getRandomBackground";
 import { ReactNode } from "react";
 
@@ -14,7 +13,7 @@ export default async function ForumLayout({
   const assets = (await getRandomBackground())?.assetsCollection.items ?? [];
   return (
     <ReactQueryProvider>
-      <div className="grid h-dvh grid-rows-[min-content_min-content_1fr_min-content_min-content] overflow-hidden">
+      <div className="grid h-dvh grid-rows-[min-content_1fr_min-content] overflow-hidden">
         <Header variant="relative" />
         {children}
         <Footer />
