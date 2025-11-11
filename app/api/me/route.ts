5c32e4809cf4dd677516a7a1fc92095485467d58
Adrien Gentizon <adrien.gentizon@gmail.com>
2025-11-11 18:50:45 +0100

    create update user pseudo route

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -1,7 +1,9 @@
 import getUser from "@/lib/auth/getUser";
 import getUserPseudo from "@/lib/auth/getUserPseudo";
+import updateUserPseudo from "@/lib/auth/updateUserPseudo";
 import { User } from "@/lib/types";
 import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 export async function GET(req: NextRequest) {
   const opertationName = `${req.method} ${req.url}`;
@@ -24,6 +26,66 @@ export async function GET(req: NextRequest) {
       },
       { status: 200 },
     );
+  } catch (error) {
+    console.error(
+      `[Operation]`,
+      opertationName,
+      (error as Error)?.message ?? error,
+    );
+    return NextResponse.json(
+      {
+        error: "server error",
+      },
+      { status: 500 },
+    );
+  }
+}
+
+export async function PATCH(req: NextRequest) {
+  const opertationName = `${req.method} ${req.url}`;
+  try {
+    console.log(`[Operation]`, opertationName);
+    const user = await getUser();
+
+    if (!user || user.bannedAt)
+      return NextResponse.json(
+        {
+          error: "unauthorized",
+        },
+        { status: 401 },
+      );
+
+    const parsedInputs = z
+      .object({
+        pseudo: z
+          .string()
+          .min(3, { message: "Pseudo trop court (3 char min)" }),
+      })
+      .safeParse(await req.json());
+
+    if (!parsedInputs.success) {
+      return NextResponse.json(
+        {
+          error: "bad request",
+        },
+        { status: 400 },
+      );
+    }
+
+    const updatedUser = await updateUserPseudo({
+      userId: user.id,
+      pseudo: parsedInputs.data.pseudo,
+    });
+
+    if (!updatedUser)
+      return NextResponse.json(
+        {
+          error: "server error",
+        },
+        { status: 500 },
+      );
+
+    return NextResponse.json<User>(updatedUser, { status: 200 });
   } catch (error) {
     console.error(
       `[Operation]`,
