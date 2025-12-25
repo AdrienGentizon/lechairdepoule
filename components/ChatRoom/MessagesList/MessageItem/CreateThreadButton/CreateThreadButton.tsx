9556eae92c0bdc87ac4e047aa4bcf026e35201cf
Jean <adrien.gentizon@gmail.com>
2025-12-25 19:43:21 +0100

    Update CreateThreadButton.tsx

--- components/ChatRoom/MessagesList/MessageItem/CreateThreadButton/CreateThreadButton.tsx
+++ components/ChatRoom/MessagesList/MessageItem/CreateThreadButton/CreateThreadButton.tsx
@@ -7,7 +7,7 @@ export default function CreateThreadButton({
 }) {
   return (
     <button
-      className="absolute right-0 inline-flex h-min items-center gap-1 rounded-b-sm border-b border-l border-r border-white bg-black px-2 text-xs opacity-0 hover:bg-gray-600 disabled:hidden group-hover:opacity-100"
+      className="absolute right-0 inline-flex h-min items-center gap-1 rounded-b-sm border-b border-l border-r border-white bg-black px-2 text-xs opacity-0 hover:bg-gray-600 disabled:hidden group-hover:opacity-100 portrait:opacity-100"
       onClick={onClick}
     >
       <Plus className="size-3" />
