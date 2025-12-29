fc34503c5d6266483d762b28c0b6c86da1f64bf4
Adrien Gentizon <adrien.gentizon@gmail.com>
2025-12-29 18:39:38 +0100

    protect api against malicious image uploads

--- lib/uploadImage.ts
+++ lib/uploadImage.ts
@@ -13,6 +13,19 @@ export default async function uploadImage(formData: FormData) {
     const file = formData.get("coverFile");
     if (!(file instanceof File)) return;
 
+    if (!file.type.startsWith("image/")) {
+      console.warn(
+        `[Warning] uploadImage: ${file.type} cannot be upload as image`
+      );
+      return;
+    }
+    if (file.size > 500 * 1024) {
+      console.warn(
+        `[Warning] uploadImage: ${file.size} exceeds ${500 * 1024} limit`
+      );
+      return;
+    }
+
     const width = getValidDimension(formData.get("coverWidth"));
     const height = getValidDimension(formData.get("coverHeight"));
     if (!width || !height) return;
