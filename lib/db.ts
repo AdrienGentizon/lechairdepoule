a7dc2c24fb97478dc11711345f95e79b61432f0b
Adrien Gentizon <adrien.gentizon@gmail.com>
2026-03-01 14:08:50 +0100

    update env

--- lib/db.ts
+++ lib/db.ts
@@ -3,7 +3,7 @@ import postgres from "postgres";
 import env from "@/lib/env";
 
 const sql = postgres(
-  `postgresql://${env().POSTGRES_USER}:${env().POSTGRES_PASSWORD}@${env().POSTGRES_HOST}/${env().POSTGRES_DATABASE}?sslmode=require&channel_binding=require`,
+  `postgresql://${env().PGUSER}:${env().PGPASSWORD}@${env().PGHOST}/${env().PGDATABASE}?sslmode=require&channel_binding=require`,
   { ssl: "require" }
 );
 
