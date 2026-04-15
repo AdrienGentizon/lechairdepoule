46e5ff8409adc73b4a69b8f1fe20a9e7312d9f5a
Adrien Gentizon <adrien.gentizon@gmail.com>
2026-04-15 11:31:04 +0200

    add feedback button

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -7,6 +7,23 @@ Sentry.init({
   dsn: "https://91ef7a2b5a550f7da751c3764c7f0aec@o4511223218700288.ingest.de.sentry.io/4511223220600912",
   tracesSampleRate: 0,
   sendDefaultPii: true,
+  integrations: [
+    Sentry.feedbackIntegration({
+      colorScheme: "light",
+      triggerLabel: "",
+      triggerAriaLabel: "Signaler un problème",
+      formTitle: "Signaler un problème",
+      submitButtonLabel: "Envoyer",
+      cancelButtonLabel: "Annuler",
+      nameLabel: "Nom",
+      namePlaceholder: "Votre nom",
+      emailLabel: "Email",
+      emailPlaceholder: "votre@email.com",
+      messageLabel: "Description",
+      messagePlaceholder: "Décrivez le problème rencontré…",
+      successMessageText: "Merci pour votre retour !",
+    }),
+  ],
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
