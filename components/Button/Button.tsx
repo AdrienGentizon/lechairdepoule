c24d4ec15b7f6db677d8a6fc1823770a4cef616f
Adrien Gentizon <adrien.gentizon@gmail.com>
2026-05-26 20:12:48 +0200

    update button hover style

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -6,7 +6,7 @@ export function buttonClassName(className?: string) {
   return cn(
     "flex w-max cursor-pointer items-center justify-center gap-2 px-8 py-0.5 text-sm font-semibold disabled:cursor-not-allowed",
     "rounded-sm border border-white bg-neutral-800 text-white transition-colors",
-    "hover:border-purple-300 hover:bg-neutral-950",
+    "hover:border-purple-300 hover:bg-neutral-950 hover:text-purple-300",
     "disabled:hover:border-neutral-300 disabled:hover:bg-neutral-800",
     className
   );
